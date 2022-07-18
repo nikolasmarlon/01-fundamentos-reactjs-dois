655b4569467e40eb856abce7ef2abd1dd92da73b
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-07-18 19:33:56 -0300

    Componente: Sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 //css module - evita que o css de um componente afete outro componente fora do seu escopo
 import styles from './Header.module.css'
 
+//assim como o css, as imagens são importadas pelo javaScript
+import igniteLogo from '../assets/ignite-logo.svg'
+
 export function Header(){
     return(
         <header className={styles.header}>
-            <strong>Header</strong>
+            <img src={igniteLogo} alt="logo do Ignite" />
+            <strong>Ignite Feed</strong>
         </header>        
     )
 }
