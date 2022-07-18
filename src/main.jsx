0f047620ab4106c7cea9a2c3a8f04722104bb8a2
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-07-18 12:09:36 -0300

    Criando um projeto React

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
+
+//importaçao do coração do react
 import React from 'react'
+
+// ReactDom é a integração do React com a DOM(Document Object Model(HTML))
 import ReactDOM from 'react-dom/client'
+
+
 import App from './App'
 
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //componente interno do React
   <React.StrictMode>
     <App />
   </React.StrictMode>
