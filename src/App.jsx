52c104f69d9d4920df82bc06bec26284288f4171
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-07-21 01:43:42 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,21 +18,15 @@ import './global.css'
 
 //Componente é uma função que retorna HTML
 export function App() { 
-
   return (
     //Para retornar multiplos elementos,  sempre precisa de um elemento pai envolvendo os elementos
 
     //autor e content são propriedade passadas para o componente Post
-    <div> 
-      
+    <div>       
       <Header />
-
-      <div className={styles.wrapper}>
-          
+      <div className={styles.wrapper}>          
           {/**Componente Sidebar */}
           <Sidebar />
-
-
           <main>
             {/*Primeiro Post (componente) passando propriedades(author, content) para o componente*/}  
             <Post author="Nikolas Marlon" content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio odit, rem natus perferendis voluptates modi accusamus suscipit nulla laborum unde obcaecati ut nihil aliquam sunt inventore porro nostrum, sequi quod."/>
