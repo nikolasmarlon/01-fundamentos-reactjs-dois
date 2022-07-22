94dd97379d393bafbee5a8a2ccfb2a3164efa663
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-07-22 00:39:13 -0300

    Inserindo comentários (programação declarativa)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBr from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
@@ -9,6 +10,36 @@ export function Post({author, content, publishedAt}){
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale: ptBr})
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {locale: ptBr, addSuffix: true, })
 
+
+    const [comments, setComments] = useState([
+        'Post Muito Bacana, hein?'
+    ])
+
+    //É sempre importante iniciar o estado com o mesmo tipo de dado que será armazenado
+    //usando programação declarativa
+    const [newCommentText, setNewCommentText] = useState('')
+
+    //função que pega o comentario dentro da textarea
+    function handleCreateNewComment(){
+        event.preventDefault() //evita que a pagina seja redirecionada ao enviar o formulário
+
+        //foi desabilitado para nao usar programação imperativa
+        //foi criado um novo estado para receber o texto da textarea (newCommentText)
+        //const newCommentText = event.target.comment.value
+
+        setComments([...comments, newCommentText])
+
+        setNewCommentText('') // voltar o estado que armazena a textarea para o valor original
+
+        //usado para limpar a textarea, porém nao é eficiente e usa programação imperativa
+        //event.target.comment.value
+    }
+
+    
+    //
+    function handleNewCommentChange(){
+        setNewCommentText(event.target.value)
+    }
     return(
         <article className={styles.post}>
             {/** Cabeçalho onde vai ficar o Avatar, cargo e a data de publicação */}
@@ -35,15 +66,19 @@ export function Post({author, content, publishedAt}){
                     } else if (line.type == 'link'){
                         return <p><a href="#">{line.content}</a></p>
                     }
-                })}
+                })} 
                 
             </div>
 
             {/**Seção formuário de comentários */}
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea placeholder='Deixe um comentário' />
+
+                {/** onChange monitorar toda vez tiver uma mudança no conteúdo da textarea vai 
+                 * chamar uma função (handleNewCommentChange)
+                 */}
+                <textarea name='comment' onChange={handleNewCommentChange} value={newCommentText} placeholder='Deixe um comentário' />
 
                 <footer>
                     <button type='submit'>Publicar</button>
@@ -53,7 +88,10 @@ export function Post({author, content, publishedAt}){
 
             {/**Lista de comentários */}
             <div className={styles.commentList}>
-                <Comment />
+                
+                {comments.map(comment => {
+                    return <Comment content={comment} />
+                })}
                
             </div>
             {/**Fim Lista de comentários */}
