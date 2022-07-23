11d1ea7313ac14ea05e6ea578c2c08dca1a5658c
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-07-22 22:21:23 -0300

    keys das listas

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -62,9 +62,9 @@ export function Post({author, content, publishedAt}){
                 
                 {content.map(line =>{
                     if(line.type == 'paragraph'){
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     } else if (line.type == 'link'){
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
                 })} 
                 
@@ -90,7 +90,7 @@ export function Post({author, content, publishedAt}){
             <div className={styles.commentList}>
                 
                 {comments.map(comment => {
-                    return <Comment content={comment} />
+                    return <Comment key={comment} content={comment} />
                 })}
                
             </div>
